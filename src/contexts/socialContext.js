bdb16e301437cffadd08b2790ecf40020e98d0c6
Avdhesh Yadav <avdhesh7394vn@gmail.com>
2024-03-30 20:53:09 +0530

    update: changed social links according to new brand name :sparkles:

--- src/contexts/socialContext.js
+++ src/contexts/socialContext.js
@@ -4,11 +4,11 @@ export const SocialContext = createContext();
 
 const SocialContextProvider = (props) => {
 	const [socials] = useState([
-		{ name: 'instagram', url: 'https://www.instagram.com/creativekonami/' },
-		{ name: 'youtube', url: 'https://www.youtube.com/channel/UCaDESR8aYy8C9nykJ6GZMYg' },
-		{ name: 'behance', url: 'https://www.behance.net/creativekonami' },
-		{ name: 'deviantart', url: 'https://www.deviantart.com/creativekonami' },
-		{ name: 'dribbble', url: 'https://dribbble.com/creativekonami' }
+		{ name: 'instagram', url: 'https://www.instagram.com/3d.artstation.okashi/' },
+		{ name: 'youtube', url: 'https://www.youtube.com/channel/UCKjfxwT4o_LEjMELV3EUlLw' },
+		{ name: 'behance', url: 'https://www.behance.net/okashibako' },
+		{ name: 'giphy', url: 'https://giphy.com/okashibako' },
+		{ name: 'dribbble', url: 'https://dribbble.com/okashibako' }
 	]);
 
 	return <SocialContext.Provider value={{ socials }}>{props.children}</SocialContext.Provider>;
